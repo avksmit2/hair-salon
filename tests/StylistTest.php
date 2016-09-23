036d1a6b1921d6d5fcd8e6ccdda4d727ff5cf99b
Angie Smith <avksmit2@gmail.com>
2016-09-23 09:17:06 -0700

    pass first test to return the stylist name

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -1,9 +1,32 @@
 <?php
-    require_once __DIR__. "/../src/Stylist.php";
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttrivutes disabled
+    */
+
+    require_once "src/Stylist.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     class StylistTest extends PHPUnit_Framework_TestCase
     {
-        
+        protected function tearDown()
+        {
+            // Stylist::deleteAll();
+        }
+
+        function test_stylistName()
+        {
+            $name = "Betty";
+            $test_stylist = new Stylist($name);
+
+            $result = $test_stylist->getName();
+
+            $this->assertEquals($name, $result);
+        }
     }
 
 
